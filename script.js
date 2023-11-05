1f34ae4c6ec9f7a6d67039b33b596dc89d05a9d3
Tsan3939 <knw930508@gmail.com>
2023-11-05 23:08:14 +0800

    Initial commit

--- script.js
+++ script.js
@@ -3,11 +3,8 @@ $(document).ready(function(){
     $("#outputButton").click(function(){
         var userInput = $("#userInput").val();
 
-        $("#outputBox").text(userInput);
-
-        $('#outputBox').removeClass('hidden');
-
-        $('#outputBox').addClass('bounce');
+        $("#outputText").text(userInput);
+        $("#outputContainer").removeClass("hidden");
     })
 });
 </script>
